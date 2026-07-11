ddac2848641732a8ab0423e1fcb19feb611b2a21
fatemabuarki77-spec <Fatemabuarki77@gmail.com>
2026-07-11 15:47:50 +0300

    app done

---
+++ server.js
@@ -0,0 +1,164 @@
+// imports
+const express = require("express"); //importing express package
+const app = express(); // creates a express application
+const dotenv = require("dotenv").config(); //this allows me to use my .env values in this file
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const Patient = require(`./models/patients`);
+app.set("view engine", "ejs");
+
+async function conntectToDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to Database");
+  } catch (error) {
+    console.log("Error Occured", error);
+  }
+}
+
+conntectToDB(); // connect to database
+
+// Middleware
+app.use(express.static("public")); //all static files are in the public folder
+app.use(express.urlencoded({ extended: false })); // this will allow us to see the data being sent in the POST or PUT
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+
+// Routes go here
+
+//create Routes:
+
+app.get("/", (req, res) => {
+  res.render("homepage.ejs");
+});
+
+app.get("/patients/new", (req, res) => {
+  res.render("patients-create.ejs");
+});
+
+app.post(`/patients`, async (req, res) => {
+  try {
+    req.body.IsReadyToDischarge = Boolean(req.body.IsReadyToDischarge);
+    console.log(req.body);
+    const createdPatient = await Patient.create(req.body);
+    console.log(createdPatient.id);
+    res.redirect(`/`);
+  } catch (err) {
+    console.log(`cannot add patient`, err);
+  }
+});
+// app.post("/patients", async (req, res) => {
+//   try {
+//     const { name, age, gender, diagnosis, IsReadyToDischarge } = req.body;
+
+//     const newPatient = {
+//       name,
+//       age,
+//       gender,
+//       diagnosis,
+//       IsReadyToDischarge: Boolean(IsReadyToDischarge),
+//     };
+
+//     const createdPatient = await Patient.create(newPatient);
+
+//     res.redirect("/");
+//   } catch (err) {
+//     console.error("Cannot add patient:", err);
+//     res.status(500).send("Error creating patient");
+//   }
+// });
+//Read Routes
+
+app.get("/patients", async (req, res) => {
+  try {
+    const allPatients = await Patient.find();
+    res.render("all-patients.ejs", { Patients: allPatients });
+  } catch (err) {
+    console.log(`cannot find patient`, err);
+  }
+});
+
+//
+app.get("/patients/:id", async (req, res) => {
+  try {
+    console.log(req.params.id);
+
+    const foundPatient = await Patient.findById(req.params.id);
+
+    console.log(foundPatient);
+    if (!foundPatient) {
+      return res.status(404).send("Patient not found");
+    }
+
+    res.render("patients-details.ejs", { Patient: foundPatient });
+  } catch (err) {
+    console.log("Cannot find patient ID", err);
+    res.status(404).send("Patient not found");
+  }
+});
+
+app.delete("/patients/:id", async (req, res) => {
+  try {
+    await Patient.findByIdAndDelete(req.params.id);
+    res.redirect("/patients");
+  } catch (err) {
+    console.log(`cannot Delete patient`, err);
+  }
+});
+
+app.get("/patients/:id/edit", async (req, res) => {
+  try {
+    const foundPatient = await Patient.findById(req.params.id);
+    res.render("edit-patients.ejs", { Patient: foundPatient });
+  } catch (err) {
+    console.log(`cannot Delete patient by id`, err);
+  }
+});
+
+app.put("/patients/:id", async (req, res) => {
+  try {
+    req.body.IsReadyToDischarge = Boolean(req.body.IsReadyToDischarge);
+    const updatedPatient = await Patient.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+    );
+    res.redirect("/patients");
+  } catch (err) {
+    console.log(`cannot Update patient`, err);
+  }
+});
+
+// app.put("/patients/:id", async (req, res) => {
+//   try {
+//     const { name, age, gender, diagnosis, IsReadyToDischarge } = req.body;
+
+//     const updatedPatient = {
+//       name,
+//       age,
+//       gender,
+//       diagnosis,
+//       IsReadyToDischarge: Boolean(IsReadyToDischarge),
+//     };
+
+//     await Patient.findByIdAndUpdate(req.params.id, updatedPatient);
+
+//     res.redirect("/patients");
+//   } catch (err) {
+//     console.error("Cannot add patient:", err);
+//     res.status(500).send("Error creating patient");
+//   }
+// });
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// 404 page ( be last)
+app.use((req, res) => {
+  res.status(404).render("404");
+});
+
+app.listen(3000, () => {
+  console.log("App is Running");
+}); // listen on port 3000
